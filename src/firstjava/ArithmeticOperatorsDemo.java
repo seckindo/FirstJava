d2528ea8fa3f0c5149faece89cf44e404f2711cf
SeckinDogaroglu <seckindo@hotmail.com>
2022-11-22 16:25:03 -0600

    First Commit

---
+++ src/firstjava/ArithmeticOperatorsDemo.java
@@ -0,0 +1,15 @@
+package firstjava;
+
+public class ArithmeticOperatorsDemo {
+
+	public static void main(String[] args) {
+		
+		int x, y = 10, z=5;
+		
+		x = y + z;
+		
+		System.out.println("+ operator resulted in " + x );
+
+	}
+
+}
