8789efcb62ea422e77e99ccbdfc6c5e6801ac56c
SeckinDogaroglu <seckindo@hotmail.com>
2022-11-22 21:13:21 -0600

    First Question

---
+++ src/firstjava/HackerRankScannerClass1.java
@@ -0,0 +1,20 @@
+package firstjava;
+
+import java.util.*;
+
+public class HackerRankScannerClass1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+	}
+
+}
